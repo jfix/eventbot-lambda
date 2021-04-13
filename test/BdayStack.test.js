9e04b2da221d1d2de7b59a61c729f6867ada4748
Jakob Fix <jakob.fix@gmail.com>
2021-04-13 23:21:44 +0200

    Rename MyStack to BdayStack

--- test/BdayStack.test.js
+++ test/BdayStack.test.js
@@ -1,11 +1,11 @@
 import { expect, haveResource } from "@aws-cdk/assert";
 import * as sst from "@serverless-stack/resources";
-import MyStack from "../lib/MyStack";
+import BdayStack from "../lib/BdayStack";
 
 test("Test Stack", () => {
   const app = new sst.App();
   // WHEN
-  const stack = new MyStack(app, "test-stack");
+  const stack = new BdayStack(app, "test-stack");
   // THEN
   expect(stack).to(haveResource("AWS::Lambda::Function"));
 });
