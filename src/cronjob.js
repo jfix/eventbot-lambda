c0bf506af0738bc4ebf06a9653fee97da5d0e87a
Jakob Fix <jakob.fix@gmail.com>
2021-05-17 22:09:55 +0200

    Clean up and add some logging

--- src/cronjob.js
+++ src/cronjob.js
@@ -13,30 +13,30 @@ tds.addRule('a', {
         node.getAttribute('href')
       )
     },
-  
     replacement: function (content, node) {
-      var href = node.getAttribute('href')
-      return '<' + content + '|' + href + '>'
+      const href = node.getAttribute('href');
+      return '<' + content + '|' + href + '>';
     }
 });
-  
+
 module.exports.handler = async () => {
     try {
-        // const date = new Date();
+        const date = new Date();
         // test date for two birthdays the same day
-        const date = new Date(2021, 4, 15);
+        // const date = new Date(2021, 4, 15);
 
-        const events = await getEvents({date})
+        const events = await getEvents({date});
 
         if (!events) {
-            console.log(`No events found for ${date}`)
-            return {}
+            console.log(`No events found for ${date}`);
+            return {};
         }
+        console.log(`HANDLER: FOUND EVENTS ${JSON.stringify(events, {}, 2)} FOR DATE: ${date}`);
         if (events.length > 1) {
-            console.log(`Unable to manage more than one event right now. :-(`)
-            console.log(`${JSON.stringify(events, {}, 2)}`)
+            console.log(`Unable to manage more than one event right now. :-(`);
+            console.log(`${JSON.stringify(events, {}, 2)}`);
         }
-        const event = events[0]
+        const event = events[0];
 
         await sendSlackMessage(process.env.EVENTBOT_SLACK_WEBHOOK_URL, {
             "unfurl_links": true,
@@ -63,7 +63,7 @@ module.exports.handler = async () => {
         });
         return {};
     } catch (error) {
-        console.log(`ERROR IN CRONJOB HANDLER: ${error}`)
+        console.log(`ERROR IN CRONJOB HANDLER: ${error}`);
         return { statusCode: 500, body: error };
     }
 };
